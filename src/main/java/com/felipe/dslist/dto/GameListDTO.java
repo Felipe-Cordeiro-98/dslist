3f8fa499d83003f57dda03ae513526ca717b41f9
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2024-11-14 14:02:43 -0300

    GET lists

---
+++ src/main/java/com/felipe/dslist/dto/GameListDTO.java
@@ -0,0 +1,25 @@
+package com.felipe.dslist.dto;
+
+import com.felipe.dslist.entities.GameList;
+
+public class GameListDTO {
+
+    private Long id;
+    private String name;
+
+    public GameListDTO() {
+    }
+
+    public GameListDTO(GameList entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
